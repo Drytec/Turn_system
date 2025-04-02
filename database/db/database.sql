2d641884beb888b9167fb251e933960b52e823d2
dylan <dylanfer830@gmail.com>
2025-04-02 11:43:34 -0500

    Re-added database folder

--- database/db/database.sql
+++
@@ -1,9 +0,0 @@
--- #######################################################
--- file:databases.sql
--- Database Course 2025
--- Univalle
--- #######################################################
-
--- Create database with turn_system
--- structure
-CREATE DATABASE turn;
