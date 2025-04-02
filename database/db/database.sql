79fe2bc857e0be9c1782aaf0f394bcd133921ee5
dylan <dylanfer830@gmail.com>
2025-04-02 11:30:57 -0500

    DB added

---
+++ database/db/database.sql
@@ -0,0 +1,9 @@
+-- #######################################################
+-- file:databases.sql
+-- Database Course 2025
+-- Univalle
+-- #######################################################
+
+-- Create database with turn_system
+-- structure
+CREATE DATABASE turn;
