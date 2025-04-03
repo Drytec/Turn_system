a2faf4e51015b9277d078fdef92b188337054215
dylan <dylanfer830@gmail.com>
2025-04-03 17:32:39 -0500

    cors added

--- backend/Desarrollo1/settings.py
+++ backend/Desarrollo1/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django.contrib.staticfiles',
+    'corsheaders',
     'apps.usuario',
     'apps.servicio',
     'apps.turno',
@@ -58,6 +59,7 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -105,8 +107,12 @@ DATABASES = {
         },
     }
 }
-
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+CORS_ORIGINS_WHITELIST = [
+    "http://localhost:3000",
+]
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
