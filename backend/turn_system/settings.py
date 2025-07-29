5ab2c0d7d9425ea0a814d89e50618148048eb093
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-07-29 09:29:46 -0500

    Update settings.py

--- backend/turn_system/settings.py
+++ backend/turn_system/settings.py
@@ -104,11 +104,6 @@ DATABASES = {
         },
     }
 }
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  
-    "http://127.0.0.1:5173",
-]
-
 
 CORS_ALLOW_CREDENTIALS = True
 # Password validation
@@ -153,3 +148,5 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
